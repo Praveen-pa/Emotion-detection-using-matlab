1f0614add6eeebf69c6e7c90239ce6dcd9223339
Praveen Annadurai <112604134+Praveen-pa@users.noreply.github.com>
2023-06-18 19:46:15 +0530

    Update readimagefromdatastore.m

--- readimagefromdatastore.m
+++ readimagefromdatastore.m
@@ -1,5 +1,18 @@
-a=read(imdsvalid  );
-class=classify(convnet,a)
+%input
+a = imread('an4.jpg');
 
-imshow(a)
+
+faceDetector = vision.CascadeObjectDetector;
+    bbox = step(faceDetector,a);
+figure,imshow(a)
+
+if ~isempty(bbox)
+    bbox=bbox(1,:);
+    rectangle('Position',bbox,'EdgeColor','b','LineWidth',3);
+end
+
+%box = imresize(a,[48 48]);
+class=classify(convnet,a);
+%imshow(box)
+%imshow(a)
 title(string(class))
